9153e04e5c6e2034c9ccb124f2880f931bf1fec3
Brian Manning <elspicyjack at gmail dot com>
2009-08-28 19:24:42 -0700

    Added some logic for switching what apps are run from xsession

--- portaboom/scripts/startx_demo.sh
+++ portaboom/scripts/startx_demo.sh
@@ -2,8 +2,15 @@
 
 # script to start X as user 'demo', with some extra housekeeping stuff
 
-SU_USER="demo"
+DEMO_HOME="/home/demo"
 STARTX="/usr/bin/startx"
 
+if [ $(/bin/grep -c DEBUG /proc/cmdline) -gt 0 ]; then
+    cat /home/demo/xsession | sed "s/^#(exec xterm.*)$/\1/" \
+        > $DEMO_HOME/.xsession
+else
+    cat /home/demo/xsession | sed "s/^#(exec perl.*)$/\1/" \
+        > $DEMO_HOME/.xsession
+fi
 /bin/su -s /bin/sh -c "$STARTX" demo
 
