42699a787c2901566ae0d2478d77b48d3c199f65
Brian Manning <brian at portaboom dot com>
2010-11-09 10:54:02 -0800

    fixed tabs/spaces and long lines issues

--- modules/wad2svg/Doom/Level.pm
+++ modules/wad2svg/Doom/Level.pm
@@ -9,72 +9,75 @@ package Doom::Level;
 use Doom::WAD qw/readlump/;
 
 sub parselinedef($) {
-	my ($from,$to,$flags,$type,$tag,$rside,$lside) = unpack("vvvvvvv",shift);
-	return { from => $from, to => $to, flags => $flags, type => $type, tag => $tag, rside => $rside, lside => $lside };
+    my ($from,$to,$flags,$type,$tag,$rside,$lside) = unpack("vvvvvvv",shift);
+    return { from => $from, to => $to, flags => $flags, type => $type,
+        tag => $tag, rside => $rside, lside => $lside };
 }
 
 my ($xmin,$ymin,$xmax,$ymax);
 
 sub parsevertex($) {
-	my ($x,$y) = unpack("ss",shift);
-	$x < $xmin and $xmin = $x;
-	$x > $xmax and $xmax = $x;
-	$y < $ymin and $ymin = $y;
-	$y > $ymax and $ymax = $y;
-	return [$x,$y];
+    my ($x,$y) = unpack("ss",shift);
+    $x < $xmin and $xmin = $x;
+    $x > $xmax and $xmax = $x;
+    $y < $ymin and $ymin = $y;
+    $y > $ymax and $ymax = $y;
+    return [$x,$y];
 }
 
 sub parsenode($) {
-	my ($x,$y,$dx,$dy,@lbbox,@rbbox,$left,$right);
-	($x,$y,$dx,$dy,@lbbox[0..3],@rbbox[0..3],$left,$right) = unpack("ssssssssssssss",shift);
-	return {
-		cut => { x => $x, y => $y, dx => $dx, dy => $dy },
-		lbbox => \@lbbox, rbbox => \@rbbox,
-		left => $left, right => $right,
-	};
+    my ($x,$y,$dx,$dy,@lbbox,@rbbox,$left,$right);
+    ($x,$y,$dx,$dy,@lbbox[0..3],@rbbox[0..3],$left,$right)
+        = unpack("ssssssssssssss",shift);
+    return {
+        cut => { x => $x, y => $y, dx => $dx, dy => $dy },
+        lbbox => \@lbbox, rbbox => \@rbbox,
+        left => $left, right => $right,
+    };
 }
 
 sub readobjects {
-	my ($map,$lump,$len,$parser) = @_;
-	my $d = readlump("$map/$lump");
-	my @l;
-	while (length $d > 0) {
-		push @l,&$parser(substr $d,0,$len);
-		$d = substr $d,$len;
-	}
-	return \@l;
+    my ($map,$lump,$len,$parser) = @_;
+    my $d = readlump("$map/$lump");
+    my @l;
+    while (length $d > 0) {
+        push @l,&$parser(substr $d,0,$len);
+        $d = substr $d,$len;
+    }
+    return \@l;
 }
 
 # Recursively turn noeds from an array into a tree
 sub treenodesfrom {
-	my ($nodearray,$n,$parent) = @_;
-	if ($n & 0x8000) { return $n & 0x7fff } # For SSector return just the number
-	my $thisnode = $nodearray->[$n];
-	$thisnode->{left} = treenodesfrom($nodearray,$thisnode->{left},$thisnode);
-	$thisnode->{right} = treenodesfrom($nodearray,$thisnode->{right},$thisnode);
-	$thisnode->{parent} = $parent;
-	return $thisnode;
+    my ($nodearray,$n,$parent) = @_;
+    if ($n & 0x8000) { return $n & 0x7fff } # For SSector return just the number
+    my $thisnode = $nodearray->[$n];
+    $thisnode->{left} = treenodesfrom($nodearray,$thisnode->{left},$thisnode);
+    $thisnode->{right} = treenodesfrom($nodearray,$thisnode->{right},$thisnode);
+    $thisnode->{parent} = $parent;
+    return $thisnode;
 }
 
 # Last node is the root
 sub treenodes($) {
-	my $nodearray = shift;
-	return treenodesfrom($nodearray,(@$nodearray - 1));
+    my $nodearray = shift;
+    return treenodesfrom($nodearray,(@$nodearray - 1));
 }
 
 sub new {
-	my $type = shift;
-	my %level;
-	$level{map} = shift || die "not enough params";
-	$level{maplump} = readlump($level{map});
-	$xmax = $ymax = -32000;
-	$xmin = $ymin = +32000;
-	$level{vertex} = readobjects($level{map},"VERTEXES",4, \&parsevertex );
-	$level{xmax} = $xmax; $level{xmin} = $xmin;
-	$level{ymax} = $ymax; $level{ymin} = $ymin;
-	$level{linedef} = readobjects($level{map},"LINEDEFS",14, \&parselinedef );
-	$level{rootnode} = treenodes(readobjects($level{map},"NODES",28, \&parsenode ));
-	return bless \%level,$type;
+    my $type = shift;
+    my %level;
+    $level{map} = shift || die "not enough params";
+    $level{maplump} = readlump($level{map});
+    $xmax = $ymax = -32000;
+    $xmin = $ymin = +32000;
+    $level{vertex} = readobjects($level{map},"VERTEXES",4, \&parsevertex );
+    $level{xmax} = $xmax; $level{xmin} = $xmin;
+    $level{ymax} = $ymax; $level{ymin} = $ymin;
+    $level{linedef} = readobjects($level{map},"LINEDEFS",14, \&parselinedef );
+    $level{rootnode}
+        = treenodes(readobjects($level{map},"NODES",28, \&parsenode ));
+    return bless \%level,$type;
 }
 
 1;
--- modules/wad2svg/Doom/WAD.pm
+++ modules/wad2svg/Doom/WAD.pm
@@ -17,34 +17,34 @@ use vars qw/@ISA @EXPORT_OK/;
 use vars qw/%lumps/;
 
 sub open ($) {
-	open(WAD,shift) or die "Failed to open: $!";
-	my $header;
-	read(WAD,$header,12) == 12 or die "Failed to read header: $!";
-	my ($sig,$lumps,$diroff) = unpack("a4VV",$header);
-	
-	seek WAD,$diroff,0 or die "Could not seek to directory: $!";
-	my $curlevel;
-	while ($lumps--) {
-		my $dirent;
-		read(WAD,$dirent,16) == 16 or die "Failed to read dir entry: $!";
-		my ($pos,$size) = unpack("VV",$dirent);
-		my $name = substr $dirent,8; $name =~ s/\0.*//;
-		if ($name =~ /^(?:THINGS|VERTEXES|LINEDEFS|SIDEDEFS|SECTORS|SSECTORS|SEGS|NODES|BLOCKMAP|REJECT)$/) {
-			$name = "$curlevel/$name";
-		} elsif ($name =~ /^(?:MAP\d\d|E\dM\d)$/) {
-			$curlevel = $name;
-		}
-		$lumps{$name} = [ $pos,$size ];
-	}
+    open(WAD,shift) or die "Failed to open: $!";
+    my $header;
+    read(WAD,$header,12) == 12 or die "Failed to read header: $!";
+    my ($sig,$lumps,$diroff) = unpack("a4VV",$header);
+
+    seek WAD,$diroff,0 or die "Could not seek to directory: $!";
+    my $curlevel;
+    while ($lumps--) {
+        my $dirent;
+        read(WAD,$dirent,16) == 16 or die "Failed to read dir entry: $!";
+        my ($pos,$size) = unpack("VV",$dirent);
+        my $name = substr $dirent,8; $name =~ s/\0.*//;
+        if ($name =~ /^(?:THINGS|VERTEXES|LINEDEFS|SIDEDEFS|SECTORS|SSECTORS|SEGS|NODES|BLOCKMAP|REJECT)$/) {
+            $name = "$curlevel/$name";
+        } elsif ($name =~ /^(?:MAP\d\d|E\dM\d)$/) {
+            $curlevel = $name;
+        }
+        $lumps{$name} = [ $pos,$size ];
+    }
 }
 
 sub readlump ($) {
-	my $l = shift || die "Must give lump name";
-	$l = $lumps{$l} || return undef;;
-	seek WAD,$l->[0],0 or return undef;
-	my $d;
-	read(WAD,$d,$l->[1]) == $l->[1] or return undef;
-	return $d;
+    my $l = shift || die "Must give lump name";
+    $l = $lumps{$l} || return undef;;
+    seek WAD,$l->[0],0 or return undef;
+    my $d;
+    read(WAD,$d,$l->[1]) == $l->[1] or return undef;
+    return $d;
 }
 
 1;
